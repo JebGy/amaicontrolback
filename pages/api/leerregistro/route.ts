8d56f2c48f829a25135d4220cba4628b8ae276f6
JebGy <martinmunayco123@hotmail.com>
2025-07-10 22:47:27 -0500

    feat: UI

--- pages/api/leerregistro/route.ts
+++ pages/api/leerregistro/route.ts
@@ -7,9 +7,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+ res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   try {
     const registros = await prisma.transaction.findMany({
       orderBy: { date: "desc" },
